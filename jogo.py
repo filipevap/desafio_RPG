21e52dde20abfa167309bd1e6d9c7591ca5bd2fb
filipevap <140600915+filipevap@users.noreply.github.com>
2026-05-21 14:23:38 -0300

    fiz umas besteiras na fase 1
    Tentativas de dar certo. ainda não rolou

--- jogo.py
+++ jogo.py
@@ -37,40 +37,47 @@ Você começa essa aventura com 'life' de {vida}pts e energia em {energia}pts"""
 vida = 100;
 energia = 50;
 
-print(f"""=====================\nVocê chegou na Ilha\n=====================
+print(f"""=====================\nFASE 1 - VOCÊ CHEGOU NA ILHA\n=====================
 Energia: {energia}
 Vida: {vida}
 
 """)
-print("""Você encontrou na praia uma cabana e um barco abandonado.
+final1 = 0
+print("""Você encontrou na praia uma CABANA e um BARCO abandonado.
 Mas a chuva está chegando e você só tem tempo para correr para um dos dois lugares.
 Pra onde você deseja ir?""")
-final1 = int(input("Cabana = 1\n\nBarco = 2")
+final1 = int(input("CABANA = 1\n\nBARCO = 2")
 
-
-
-#cabana ou barco
-#FASE 2 CABANA 1
-print("=====================\nFASE 2\n=====================")
-
-if fase1 == 1:
+####
+             
+if final1 != 1  
+    fase2, final1 = int(input(f"""Caraca.. Você se deparou com um variável virulenta totalmente virulosa.
+    Ela acabou te virulando e você terminou descobrindo que deveria ter ido para o barco.
+    Digite '2' para ir para o BARCO"""))
+final1 = 0
+             
+else final1 == 1:
     energia -= 10
     vida = 100
-    print(f"Energia: {energia}\nVida: {vida}")
+print(f"""Energia: {energia}
+Vida: {vida}
+""")
     
-    fase2 = int(input("Caraca.. Você se deparou com um variável virulenta totalmente virulosa e que acabou te virulando e você acabou descobrindo que deve ir para o barco. Digite '2'"))
-
+    
 #FASE 2 BARCO 2
 else:
     print("=====================\nFASE 2\n=====================")
     energia -= 10
     vida = 100
-    print(f"Energia: {energia}\nVida: {vida}")
+print(f"""Energia: {energia}
+Vida: {vida}
+""")
+
     fase2 = int(input("Ao chegar no barco você encontrou uma faca e um espelho. Contudo, pode escolher apenas um dos itens pois está com os bolsos cheios. Escolha apenas 1\nFaca - 3            Espelho - 4"))
 
 oioioi
 
-
+#=========================================================   FASE 2   ============================================================
 #FASE 3 - faca e espelho
 if fase2 == 2: #ESCOLHA FACA
     print("=====================\nFASE 2\n=====================")
@@ -78,7 +85,11 @@ if fase2 == 2: #ESCOLHA FACA
     vida = 100
     print(f"Energia: {energia}\nVida: {vida}")
 
-    fase3 = int(input("Ao chegar no barco você encontrou uma faca e um espelho. Contudo, pode escolher apenas um dos itens pois está com os bolsos cheios. Escolha apenas 1\nFaca - 3            Espelho - 4"))
+    fase3 = int(input("""Ao chegar no barco você encontrou uma faca e um espelho.
+    Contudo, pode escolher apenas um dos itens pois está com os bolsos cheios.
+    Escolha apenas 1.
+    Faca - 3
+    Espelho - 4"""))
 
 elif fase3 == 3:
     print("=====================\nFASE 2\n=====================")
@@ -90,11 +101,11 @@ elif fase3 == 3:
 
 else:
     print("escolheu o espelho")
-#FASE 4
+#=========================================================   FASE 2   ============================================================
 
 energia -= 10
 
 
-#FASE 5
+#=========================================================   FASE 2   ============================================================
 
 energia -= 10
